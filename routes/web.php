e5808ae389c1fa47e01a65daf591b2d8f871b25e
Nadeem Ahmed <aknadeemahmed@gmail.com>
2021-12-06 12:33:28 +0500

    theme setup

--- routes/web.php
+++ routes/web.php
@@ -3,20 +3,12 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UserManagement\{ UserController, UserLevelController};
-use App\Http\Controllers\PoultryShed\ {PoultryShedController, EmployeeController};
-use App\Http\Controllers\PartyManagement\ {
-    CustomerController,CompaniesController, CompaniesBalanceController };
-
-use App\Http\Controllers\InventoryManagement\ {FeedController, ExpenseController};
-use App\Http\Controllers\ChickenModule\ {ChickenPurchaseController, ChickenSaleController};
-
 Auth::routes();
 
 Route::group(['middleware' => 'auth'], function(){
 	Route::get('/', function () {
         return view('welcome');
     });
-
     Route::group(['prefix' => '/usermanagement'], function(){
         Route::get('/get-users-list', [UserController::class, 'getUsersList'])->name('getUsersList');
         Route::get('/get-userlevels', [UserController::class, 'getUserLevelList'])->name('getUserLevelList');
@@ -25,51 +17,4 @@ Route::group(['middleware' => 'auth'], function(){
     });
 
     Route::get('/home', [HomeController::class, 'index'])->name('home');
-    Route::resource('poultryshed', PoultryShedController::class);
-    Route::resource('employee', EmployeeController::class)->except([
-        'create', 'update'
-    ]);
-
-    Route::get('/getEmployeeList', [EmployeeController::class, 'getEmployeeList'])->name('getEmployeeList');
-    Route::resource('customer', CustomerController::class)->except([
-        'create', 'update'
-    ]);
-
-    Route::get('/getCompaniesList', [CompaniesController::class, 'getCompaniesList'])->name('getCompaniesList');
-    
-    Route::resource('company', CompaniesController::class)->except([
-        'create', 'update'
-    ]);
-
-    Route::get('/getCompaniesBalanceList', [CompaniesBalanceController::class, 'getCompaniesBalanceList'])->name('getCompaniesBalanceList');
-    
-    Route::resource('companybalance', CompaniesBalanceController::class)->except([
-        'create', 'update'
-    ]);
-    Route::group(['prefix' => '/inventory'], function(){
-        Route::get('/get-feed-list', [FeedController::class, 'getFeedList'])->name('getfeedlist');
-
-        Route::resource('feed', FeedController::class)->except([
-            'create', 'update'
-        ]); 
-        
-        Route::get('/expense-categories', [ExpenseController::class, 'getExpenseCategoryList'])->name('getExpenseCategoryList');
-
-        Route::get('/expense-list', [ExpenseController::class, 'getExpenseList'])->name('getExpenseList');
-        
-        Route::resource('expense', ExpenseController::class)->except([
-            'create', 'update'
-        ]);
-    });
-
-    Route::group(['prefix' => '/poultry'], function(){
-        Route::get('/get-purchase-list', [ChickenPurchaseController::class, 'getPurchaseList'])->name('getpurchaselist');
-        Route::resource('purchase', ChickenPurchaseController::class);
-    });
-
-    Route::group(['prefix' => '/poultry'], function(){
-        Route::get('/get-sales-list', [ChickenSaleController::class, 'getSalesList'])->name('getSalesList');
-        Route::resource('sale', ChickenSaleController::class);
-    });
-
 });
